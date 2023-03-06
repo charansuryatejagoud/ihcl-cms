d852d771b629032c13f121383c367ffd4ccac106
Sukumar-K7 <sukumar.k@techouts.co.in>
2023-03-06 18:20:33 +0530

    changed nomenclature case

--- schemas/hotel-details/index.ts
+++ schemas/hotel-details/index.ts
@@ -4,103 +4,103 @@ export const details: FeatureSchemaDefinition = {
   variants: {
     group: [
       {
-        title: "[details] 3 Card Carousel",
+        title: "[Details] 3 Card Carousel",
         value: "details.group.3-card-carousel",
       },
       {
-        title: "[details] Group With Rich Text Columns",
+        title: "[Details] Group With Rich Text Columns",
         value: "details.group.group-with-rich-text-columns",
       },
       {
-        title: "[details] Group With Maps",
+        title: "[Details] Group With Maps",
         value: "details.group.group-with-maps",
       },
       {
-        title: "[details] Carousel With side Text Card",
+        title: "[Details] Carousel With side Text Card",
         value: "details.group.carousel-with-side-text-card",
       },
       {
-        title: "[details] Group With 2 Column Cards Grid",
+        title: "[Details] Group With 2 Column Cards Grid",
         value: "details.group.group-with-2-column-cards-grid",
       },
       {
-        title: "[details] Group With Carousel-Card Left Media Right Content",
+        title: "[Details] Group With Carousel-Card Left Media Right Content",
         value:
           "details.group.group-with-carousel-card-left-media-right-content-aspect-ratio-2:4",
       },
       {
-        title: "[details] Group With Card Left Media Right Content",
+        title: "[Details] Group With Card Left Media Right Content",
         value:
           "details.group.group-with-card-left-media-right-content-aspect-ratio-2:4",
       },
       {
-        title: "[details] Group With Carousel- Card right Media Left Content",
+        title: "[Details] Group With Carousel- Card right Media Left Content",
         value:
           "details.group.group-with-carousel-card-right-media-left-content-aspect-ratio-2:4",
       },
       {
-        title: "[details] Group With Card right Media Left Content",
+        title: "[Details] Group With Card right Media Left Content",
         value:
           "details.group.group-with-card-right-media-left-content-aspect-ratio-2:4",
       },
       {
-        title: "[details] Group with Full Width Media",
+        title: "[Details] Group with Full Width Media",
         value: "details.group.group-with-full-width-media",
       },
       {
-        title: "[details] Navigation Tabs",
+        title: "[Details] Navigation Tabs",
         value: "details.group.navigation-tabs",
       },
       {
-        title: "[details] Tab Dropdown Selector",
+        title: "[Details] Tab Dropdown Selector",
         value: "details.group.tab-dropdown-selector",
       },
       {
-        title: "[details] Center Card Carousel",
+        title: "[Details] Center Card Carousel",
         value: "details.group.center-card-carousel",
       },
     ],
     card: [
       {
-        title: "[details] Award Card",
+        title: "[Details] Award Card",
         value: "details.card.award-card",
       },
       {
-        title: "[details] Card With Side Text",
+        title: "[Details] Card With Side Text",
         value: "details.card.card-with-side-text",
       },
       {
-        title: "[details] 3 Actions With Right Aligned Content Card",
+        title: "[Details] 3 Actions With Right Aligned Content Card",
         value: "details.card.3-actions-with-right-aligned-content-card",
       },
       {
-        title: "[details] Card With Right Aligned Content",
+        title: "[Details] Card With Right Aligned Content",
         value: "details.card.card-with-right-aligned-content",
       },
       {
-        title: "[details] Card With  Left Media Right Content",
+        title: "[Details] Card With  Left Media Right Content",
         value:
           "details.card.card-with-left-media-right-content-aspect-ratio-2:4",
       },
       {
-        title: "[details] Card With  Right Media Left Content",
+        title: "[Details] Card With  Right Media Left Content",
         value:
           "details.card.card-with-right-media-left-content-aspect-ratio-2:4",
       },
       {
-        title: "[details] Card with Left Align Content",
+        title: "[Details] Card with Left Align Content",
         value: "details.card.left-align-content",
       },
     ],
     banner: [],
     dialog: [
-      { title: "[details] Image Gallery", value: "details.image-gallery" },
-      { title: "[details] Venue Enquiry", value: "details.venue-enquiry" },
-      { title: "[details] Event Enquiry", value: "details.event-enquiry" },
-      { title: "[details] Venue Details", value: "details.venue-details" },
-      { title: "[details] Video Pop Up", value: "details.video-pop-up" },
+      { title: "[Details] Image Gallery", value: "details.image-gallery" },
+      { title: "[Details] Venue Enquiry", value: "details.venue-enquiry" },
+      { title: "[Details] Event Enquiry", value: "details.event-enquiry" },
+      { title: "[Details] Venue Details", value: "details.venue-details" },
+      { title: "[Details] Video Pop Up", value: "details.video-pop-up" },
       {
-        title: "[details] Gallery With Carousel",
+        title: "[Details] Gallery With Carousel",
         value: "details.gallery-with-carousel",
       },
     ],
--- schemas/ihcl-loyalty/index.ts
+++ schemas/ihcl-loyalty/index.ts
@@ -5,70 +5,70 @@ export const loyalty: FeatureSchemaDefinition = {
   variants: {
     group: [
       {
-        title: "[loyalty] Group With tiers",
+        title: "[Loyalty] Group With tiers",
         value: "loyalty.group-with-tiers",
       },
       {
-        title: "[loyalty] Group Preview Carousel",
+        title: "[Loyalty] Group Preview Carousel",
         value: "loyalty.group-preview-carousel",
       },
       {
-        title: "[loyalty] Tier Card with Right Aligned Content",
+        title: "[Loyalty] Tier Card with Right Aligned Content",
         value: "loyalty.tier-card-with-right-aligned-content",
       },
       {
-        title: "[loyalty] [rectangle] 4 Card Carousel",
+        title: "[Loyalty] [rectangle] 4 Card Carousel",
         value: "loyalty.4-card-carousel",
       },
-      { title: "[loyalty] 3 Row Grid", value: "loyalty.3-row-grid" },
+      { title: "[Loyalty] 3 Row Grid", value: "loyalty.3-row-grid" },
       { title: "[loyalty] 2 Card Carousel", value: "loyalty.2-card-carousel" },
       {
-        title: "[loyalty] Comperative Specifications",
+        title: "[Loyalty] Comperative Specifications",
         value: "loyalty.group-with-comperative-specifications",
       },
       {
-        title: "[loyalty] Group With Group Of Gift Items Right Content",
+        title: "[Loyalty] Group With Group Of Gift Items Right Content",
         value: "loyalty.group.group-of-gift-items-right-content",
       },
       {
-        title: "[loyalty] Group with Card Media Content Positions",
+        title: "[Loyalty] Group with Card Media Content Positions",
         value: "loyalty.group.card-media-content-positions",
       },
     ],
     card: [
-      { title: "[loyalty] Text on Image", value: "loyalty.text-on-image" },
+      { title: "[Loyalty] Text on Image", value: "loyalty.text-on-image" },
       {
-        title: "[loyalty] Card with Image Title",
+        title: "[Loyalty] Card with Image Title",
         value: "loyalty.card.card-with-image-title",
       },
       {
-        title: "[loyalty] Card With Combined Image Button",
+        title: "[Loyalty] Card With Combined Image Button",
         value: "loyalty.image-aligned-button",
       },
       {
-        title: "[loyalty] Text on Card with Cta",
+        title: "[Loyalty] Text on Card with Cta",
         value: "loyalty.text-on-card-with-cta",
       },
       {
-        title: "[loyalty] Card With Right Aligned Carousel",
+        title: "[Loyalty] Card With Right Aligned Carousel",
         value: "loyalty.card-with-right-aligned-carousel",
       },
       {
-        title: "[loyalty] Card With Group Of Gift Items Right Content",
+        title: "[Loyalty] Card With Group Of Gift Items Right Content",
         value: "loyalty.card.card-with-group-of-gift-items-right-content",
       },
       {
-        title: "[loyalty] Card With Hotel Details",
+        title: "[Loyalty] Card With Hotel Details",
         value: "loyalty.card.card-with-hotel-details",
       },
     ],
     nudge: [
       {
-        title: "[loyalty] Nudge With Key Value",
+        title: "[Loyalty] Nudge With Key Value",
         value: "loyalty.nudge-with-key-value",
       },
       {
-        title: "[loyalty] Image Preview Nudge",
+        title: "[Loyalty] Image Preview Nudge",
         value: "loyalty.imagePreviewNudge",
       },
       {
@@ -76,24 +76,24 @@ export const loyalty: FeatureSchemaDefinition = {
         value: "loyalty.nudge-with-action",
       },
       {
-        title: "[loyalty] Nudge Dual Action",
+        title: "[Loyalty] Nudge Dual Action",
         value: "loyalty.nudge.dual-action",
       },
       {
-        title: "[loyalty] Nudge With Mail Action",
+        title: "[Loyalty] Nudge With Mail Action",
         value: "loyalty.nudge.rounded.mail-action",
       },
     ],
     dialog: [
-      { title: "[loyalty] Manage Card", value: "manage-card" },
+      { title: "[Loyalty] Manage Card", value: "manage-card" },
       {
-        title: "[loyalty] Enquire Bulk Gift Card",
+        title: "[Loyalty] Enquire Bulk Gift Card",
         value: "enquire-bulk-gift-card",
       },
     ],
     stepper: [
       {
-        title: "[loyalty] E-Gift Card Flow",
+        title: "[Loyalty] E-Gift Card Flow",
         value: "loyalty.stepper.e-gift-card-flow",
       },
     ],
