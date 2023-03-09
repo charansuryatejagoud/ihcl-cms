ea19679e7cdbc3a04f150e9038adf7ba4f5b4e1c
Ravulavamsikrishna <vamshikrishna.r@techouts.com>
2023-03-09 13:31:46 +0530

    Navigation Items file formated

--- schemas/core/objects/navigationItem.ts
+++ schemas/core/objects/navigationItem.ts
@@ -19,39 +19,38 @@ export default {
       // validation: pathUrlRule,
     },
     {
-      name:"image",
-      title:"Image",
-      type:"image"
+      name: "image",
+      title: "Image",
+      type: "image",
     },
     {
       name: "variant",
       title: "Variant",
       type: "string",
       options: {
-        list: [ 
-                 { title: "Light-Contained", value: "light-contained" },
-                 { title: "Dark-Contained", value: "dark-contained" },
-                 { title: "Light-Outlined", value: "light-outlined" },
-                 { title: "Dark-Outlined", value: "dark-outlined" },
-                 { title: "Link", value: "link-m" },
-                 
-              ]
-         }
+        list: [
+          { title: "Light-Contained", value: "light-contained" },
+          { title: "Dark-Contained", value: "dark-contained" },
+          { title: "Light-Outlined", value: "light-outlined" },
+          { title: "Dark-Outlined", value: "dark-outlined" },
+          { title: "Link", value: "link-m" },
+        ],
+      },
     },
     {
-      title: 'Check Box',
-      name: 'checkBox',
-       type: 'string',
+      title: "Check Box",
+      name: "checkBox",
+      type: "string",
       options: {
-          list: [
-              { title: 'Internal',value: 'internal' },
-              { title: 'External',value: 'external' },
-              {title:"Dialog",value:"dialog"}
-          ],
-          layout:'radio',
-          isHighlighted:true
-      }
-  },
+        list: [
+          { title: "Internal", value: "internal" },
+          { title: "External", value: "external" },
+          { title: "Dialog", value: "dialog" },
+        ],
+        layout: "radio",
+        isHighlighted: true,
+      },
+    },
   ],
   preview: {
     select: {
