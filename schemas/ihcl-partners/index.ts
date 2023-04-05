8debf825b6bc310043dd2b4860ea20c33c6fc351
Sukumar-K7 <sukumar.k@techouts.co.in>
2023-04-05 16:45:41 +0530

    rectified nomenclature

--- schemas/ihcl-partners/index.ts
+++ schemas/ihcl-partners/index.ts
@@ -5,13 +5,13 @@ export const partners: FeatureSchemaDefinition = {
     group: [],
     card: [
       {
-        title: "[partners] Right Media Image Carousel with Left Content",
+        title: "[Partners] Right Media Image Carousel with Left Content",
         value: "partners.card.right-media-image-carousel-with-left-content",
       },
     ],
     nudge: [
       {
-        title: "[partners] Left Image Right Content With Border",
+        title: "[Partners] Left Image Right Content With Border",
         value: "partners.nudge.left-image-right-content-with-border",
       },
     ],
