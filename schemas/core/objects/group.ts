620ed1105ddf4463c0737df86d666c4120ab090c
Sukumar-K7 <sukumar.k@techouts.co.in>
2023-06-06 13:11:44 +0530

    renamed the branch

--- schemas/core/objects/group.ts
+++ schemas/core/objects/group.ts
@@ -4,22 +4,6 @@ import { hiddenField } from "../../shared-utils";
 import CustomText from "../../../components/custom-text/index";
 
 const AlignmentVariant = [
-  // {
-  //   title: "Regular With Two Row Title",
-  //   value: "regular-with-two-row-title",
-  // },
-  // {
-  //   title: "Regular With One Row Title",
-  //   value: "regular-with-one-row-title",
-  // },
-  // {
-  //   title: "Center With One Row Title",
-  //   value: "center-with-one-row-title",
-  // },
-  // {
-  //   title: "Center With Multi Line Title",
-  //   value: "center-with-multi-line-title",
-  // },
   {
     title: "Center",
     value: "center",
