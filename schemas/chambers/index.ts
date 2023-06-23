4969b13854471e4fbae048255c063dc55abd6fd7
Sukumar-K7 <sukumar.k@techouts.co.in>
2023-06-23 17:06:10 +0530

    updated variants

--- schemas/chambers/index.ts
+++ schemas/chambers/index.ts
@@ -4,16 +4,12 @@ export const chambers: FeatureSchemaDefinition = {
   variants: {
     card: [
       {
-        title:
-          "[Chambers] Left Vertical Media With Bullet Line and Right Content",
-        value:
-          "chambers.card.left-vertical-media-with-bullet-line-and-right-content",
+        title: "[Chambers] Details",
+        value: "chambers.card.details",
       },
       {
-        title:
-          "[Chambers] Right Vertical Media With Bullet Line and Left Content",
-        value:
-          "chambers.card.right-vertical-media-with-bullet-line-and-left-content",
+        title: "[Chambers] Reverse Details",
+        value: "chambers.card.reverse-details",
       },
     ],
   },
