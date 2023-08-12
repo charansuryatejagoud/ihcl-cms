32f458f1d6a763dfc6390106486146aad7a60ad8
Venkata Raghu <venkataraghu@techouts.com>
2023-08-12 19:17:32 +0530

    offers schema changes

--- schemas/content-fragments/offers/index.ts
+++ schemas/content-fragments/offers/index.ts
@@ -1,7 +1,8 @@
 import { FeatureSchemaDefinition } from "schemas/types";
 import offerPackages from "./offerPackages";
 import offerThemes from "./offerThemes";
+import dateRange from "../dateRange";
 
 export const offersAndPromotions: FeatureSchemaDefinition = {
-  schemas: [offerPackages, offerThemes],
+  schemas: [offerPackages, offerThemes, dateRange],
 };
