81fb5eaafd7ffa7c729fccdfdffba06121d42cc2
NarsingRao13 <narsingrao.k@techouts.com>
2023-08-31 20:27:34 +0530

    Code updated

--- plugins/read-excel/Contact.tsx
+++ plugins/read-excel/Contact.tsx
@@ -2,16 +2,7 @@ import React, { useRef, useState } from "react";
 import { Button, Flex } from "@sanity/ui";
 import * as XLSX from "xlsx";
 import { customAlphabet } from "nanoid";
-import sanityClient from "@sanity/client";
-
-const client = sanityClient({
-  projectId: "ocl5w36p",
-  dataset: "production",
-  apiVersion: "v2021-10-21",
-  token:
-    "skIlzYEV0AyovwCGKc4uvF7kNe3IdAp3zI4yjdqSBAB9gpj9r4GnsCmYh9o7iRe9htOJCKdLiJBLpjAFnedjFoLiKujs6mvSmwzkvr0t5obhmsh6Gb6s0MOnarAkqzRikYgBYNkZdEEc7v8BtvywajXtW9A4DmxeZ41aYnJbowf8XOPVt5vc",
-  useCdn: false,
-});
+import { client } from "./client";
 
 function Contact() {
   const ref: any = useRef();
