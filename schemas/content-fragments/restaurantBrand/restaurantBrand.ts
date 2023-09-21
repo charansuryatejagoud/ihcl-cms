ac75771931839d0ca70c9f99b60040ad823d6d05
Krishna Vamshi <vamshikrishna.t@techouts.com>
2023-09-21 14:00:54 +0530

    Added SEO for Restaurant Brands

--- schemas/content-fragments/restaurantBrand/restaurantBrand.ts
+++ schemas/content-fragments/restaurantBrand/restaurantBrand.ts
@@ -92,5 +92,21 @@ export default {
         },
       ],
     },
+    {
+      name: "pageTitle",
+      title: "Page Title",
+      type: "string",
+    },
+    {
+      name: "seoKeywords",
+      title: "SEO Keywords",
+      type: "string",
+    },
+    {
+      name: "seoDescription",
+      title: "SEO Description",
+      type: "text",
+      rows: 5,
+    }
   ],
 };
