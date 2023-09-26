5d04fbb774abcb877eb2c817d35a87d627a92b7b
NarsingRao13 <79701984+NarsingRao13@users.noreply.github.com>
2023-09-26 18:31:57 +0530

    code updated

--- plugins/delete-unused-asserts/deleteUnusedAssets.js
+++ plugins/delete-unused-asserts/deleteUnusedAssets.js
@@ -10,26 +10,26 @@ import { client } from "../read-excel/client";
 
 const query = `*[ _type in ["sanity.imageAsset", "sanity.fileAsset"] ] 
 {_id, "refs": count(*[ references(^._id) ])}
-[ refs == 0 ][0..99]._id`              
+[ refs == 0 ][0..99]._id`;
 
 client
   .fetch(query)
-  .then(ids => {
+  .then((ids) => {
     if (!ids.length) {
-      console.log('No assets to delete')
-      return true
+      console.log("No assets to delete");
+      return true;
     }
-    console.log(`Deleting ${ids} assets`)
-     return ids
-        .reduce((trx, id) => trx.delete(id), client.transaction())
-        .commit()
-        .then(() => console.log('Done!'))
+    console.log(`Deleting ${ids} assets`);
+    return ids
+      .reduce((trx, id) => trx.delete(id), client.transaction())
+      .commit()
+      .then(() => console.log("Done!"));
   })
-  .catch(err => {
-    if (err.message.includes('Insufficient permissions')) {
-      console.error(err.message)
-      console.error('Did you forget to pass `--with-user-token`?')
+  .catch((err) => {
+    if (err.message.includes("Insufficient permissions")) {
+      console.error(err.message);
+      console.error("Did you forget to pass `--with-user-token`?");
     } else {
-      console.error(err.stack)
+      console.error(err.stack);
     }
-  })
+  });
