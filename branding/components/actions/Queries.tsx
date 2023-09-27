c4db8f42f8d2bf901a893337a9420f05695e61c2
Venkata Raghu <venkataraghu@techouts.com>
2023-09-28 00:56:52 +0530

    updated query with hotel highlights

--- branding/components/actions/Queries.tsx
+++ branding/components/actions/Queries.tsx
@@ -34,7 +34,7 @@ export const queries = {
        hotelId, hotelName, brandName,hotelPath,identifier,
        "hotelImage":hotelOverview->basicInfo.media[0].imageAsset.largeImage[0].asset->url,
        "longDescription":hotelOverview->basicInfo.description,
-       "hotelHighlights":hotelHighlights->highlightDetails[].basicInfo.title,
+       "hotelHighlights":hotelFacilities->facilityDetails[title match "Hotels"].list[].item,
             hotelAddress->{
               addressLine1,city,state,country,pincode,
               latitude,longitude,regionKey
