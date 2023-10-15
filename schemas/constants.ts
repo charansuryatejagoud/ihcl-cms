2eeb26b6e34d61fd74948e3a436050119726c266
Sukumar <venkataraghu@techouts.co.in>
2023-10-15 20:00:38 +0530

    added weddingVenues contentType

--- schemas/constants.ts
+++ schemas/constants.ts
@@ -361,6 +361,10 @@ export const contentTypes = [
     title: "Opening Soon(Non-Taj)",
     value: "openingSoonNonTaj",
   },
+  {
+    title: "Wedding Venues",
+    value: "weddingVenues",
+  }
 ];
 export const filterAlignmentTypes = [
   {
